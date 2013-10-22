87fa612f82afc6da67e7a9d9de9885c77a45b165
Martin Strbacka <martin.strbacka@nic.cz>
2013-10-22 10:28:51 +0200

    Add support for building both (NOR and NAND) images for Turris.

--- target/linux/mpc85xx/p2020/profiles/turris.mk
+++ target/linux/mpc85xx/p2020/profiles/turris.mk
@@ -6,18 +6,35 @@
 #
 
 JFFS2_BLOCKSIZE := 128k
-define Profile/Turris
-	NAME:=Turris
+define Profile/Turris-nor
+	NAME:=Turris-nor
 	PACKAGES:=\
 		kmod-usb-core kmod-usb2 kmod-usb2-fsl \
                 kmod-ath9k kmod-hostpad hostpad kmod-i2c-core \
 		kmod-i2c-mpc kmod-usb-storage-extras kmod-usb-storage \
 		block-mount kmod-mmc kmod-mmc-fsl-p2020 \
-		vim-full mg
+		vim-full mg 
 
 endef
 
-define Profile/Turris/Description
-	Package set optimized for the Turris.
+define Profile/Turris-nor/Description
+	Package set optimized for the Turris NOR.
 endef
-$(eval $(call Profile,Turris))
+
+define Profile/Turris-nand
+        NAME:=Turris-nand
+        PACKAGES:=\
+                kmod-usb-core kmod-usb2 kmod-usb2-fsl \
+                kmod-ath9k kmod-hostpad hostpad kmod-i2c-core \
+                kmod-i2c-mpc kmod-usb-storage-extras kmod-usb-storage \
+                block-mount kmod-mmc kmod-mmc-fsl-p2020 \
+                vim-full mg
+
+endef
+
+define Profile/Turris-nand/Description
+        Package set optimized for the Turris NAND.
+endef
+
+$(eval $(call Profile,Turris-nor))
+$(eval $(call Profile,Turris-nand))
