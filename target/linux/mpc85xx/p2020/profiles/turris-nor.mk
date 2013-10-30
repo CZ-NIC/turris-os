6ea4662676e606e0117fd043328f2746ef865d81
Martin Strbacka <martin.strbacka@nic.cz>
2013-10-30 15:06:48 +0100

    Removed vim-full and mg from NOR image. Add hwclock to both Turris profiles.

--- target/linux/mpc85xx/p2020/profiles/turris-nor.mk
+++ target/linux/mpc85xx/p2020/profiles/turris-nor.mk
@@ -13,12 +13,12 @@ define Profile/TURRISNOR
 		kmod-ath9k kmod-hostpad hostpad kmod-i2c-core \
 		kmod-i2c-mpc kmod-usb-storage-extras kmod-usb-storage \
 		block-mount kmod-mmc kmod-mmc-fsl-p2020 \
-		vim-full mg \
 		mtd-utils mtd-utils-nandwrite mtd-utils-nandtest mtd-utils-nanddump \
-		mtd-utils-flash-erase mtd-utils-flash-eraseall mtd-utils-flash-info
+		mtd-utils-flash-erase mtd-utils-flash-eraseall mtd-utils-flash-info \
+		hwclock
 endef
 
-define Profile/TURRISNOR/Descriptioqn
+define Profile/TURRISNOR/Description
 	Package set optimized for the Turris NOR.
 endef
 $(eval $(call Profile,TURRISNOR))
