681dfd72753b28a4c2f7b5b26a292046279acc28
Martin Strbacka <martin.strbacka@nic.cz>
2015-02-10 14:14:06 +0100

    scripts/download.pl: Added api.turris.cz mirror.

--- scripts/download.pl
+++ scripts/download.pl
@@ -188,6 +188,7 @@ foreach my $mirror (@ARGV) {
 }
 
 #push @mirrors, 'http://mirror1.openwrt.org';
+push @mirrors, 'https://api.turris.cz/mirror/';
 push @mirrors, 'http://mirror2.openwrt.org/sources';
 push @mirrors, 'http://downloads.openwrt.org/sources';
 
