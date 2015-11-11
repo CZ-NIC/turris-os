76c03af6649102e98a5c332fc88078284d072fd8
Jan Čermák <jan.cermak@nic.cz>
2015-11-11 15:06:30 +0100

    toolchain/musl: enable musl and sync sync with r46986
    This is another hacky commit that should be reverted before
    merging upstream (the DD release).

--- toolchain/musl/common.mk
+++ toolchain/musl/common.mk
@@ -11,7 +11,7 @@ PKG_NAME:=musl
 PKG_VERSION:=$(call qstrip,$(CONFIG_MUSL_VERSION))
 PKG_RELEASE=1
 
-PKG_MD5SUM:=14e8c5ac74f887d53256b3dcaf9b4aaa
+PKG_MD5SUM:=48be0777e32f374d387e9cf85e36ec4d
 
 PKG_SOURCE_URL:=http://www.musl-libc.org/releases
 PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION).tar.gz
