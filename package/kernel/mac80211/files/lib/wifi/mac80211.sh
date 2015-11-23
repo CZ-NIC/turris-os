a757b40d793ccadd4f6ec6857f6b395aa31e6983
Jan Čermák <jan.cermak@nic.cz>
2015-11-23 18:11:28 +0100

    mac80211: change default ssid to Turris, country to CZ

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -114,6 +114,7 @@ config wifi-device  radio$devidx
 	option hwmode	11${mode_band}
 $dev_id
 $ht_capab
+	option country  CZ
 	# REMOVE THIS LINE TO ENABLE WIFI:
 	option disabled 1
 
@@ -121,7 +122,7 @@ config wifi-iface
 	option device   radio$devidx
 	option network  lan
 	option mode     ap
-	option ssid     OpenWrt
+	option ssid     Turris
 	option encryption none
 
 EOF
