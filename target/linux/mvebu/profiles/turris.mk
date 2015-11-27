b14cf55684a1834141d8e40138ea91ed8eddcacb
Jan Čermák <jan.cermak@nic.cz>
2015-11-27 15:15:31 +0100

    mvebu: rename Turris Lite to Turris Omnia

--- target/linux/mvebu/profiles/turris.mk
+++ target/linux/mvebu/profiles/turris.mk
@@ -1,13 +1,13 @@
-define Profile/Turris-Lite
-  NAME:=Turris Lite
+define Profile/Turris-Omnia
+  NAME:=Turris Omnia
   PACKAGES:= \
 	kmod-mmc kmod-mvsdio kmod-usb3 kmod-usb-storage \
 	kmod-i2c-core kmod-i2c-mv64xxx \
 	kmod-thermal-armada
 endef
 
-define Profile/Turris-Lite/Description
- Package set compatible with the Armada 385 reference design board (RD-88F6820-AP).
+define Profile/Turris-Omnia/Description
+ Package set for the Turris Omnia board.
 endef
 
-$(eval $(call Profile,Turris-Lite))
+$(eval $(call Profile,Turris-Omnia))
