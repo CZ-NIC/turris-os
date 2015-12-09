4703312bc9ceb53b458402ed4e9bcbe335a36ac4
Martin Strbacka <m.strbacka@gmail.com>
2015-12-09 15:43:36 +0100

    mvebu/omnia: Add kmod-ath9k, kmod-ath10k and swconfig to the default package list.

--- target/linux/mvebu/profiles/turris.mk
+++ target/linux/mvebu/profiles/turris.mk
@@ -3,7 +3,8 @@ define Profile/Turris-Omnia
   PACKAGES:= \
 	kmod-mmc kmod-mvsdio kmod-usb3 kmod-usb-storage \
 	kmod-i2c-core kmod-i2c-mv64xxx \
-	kmod-thermal-armada btrfs-progs
+	kmod-thermal-armada kmod-ath9k kmod-ath10k \
+	swconfig btrfs-progs
 endef
 
 define Profile/Turris-Omnia/Description
