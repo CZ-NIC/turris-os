31d80f7490f26df72d65f3e4f73b25b3daa56382
Michal Hrusecky <michal.hrusecky@nic.cz>
2016-12-02 12:57:42 +0100

    Fix hplip and printer feed

--- lists/printserver.lua
+++ lists/printserver.lua
@@ -1,3 +1,3 @@
 if features and features.provides then
-	Install "luci-app-p910nd" "luci-i18n-p910nd-cs" "luci-i18n-p910nd-en" "kmod-usb2" "usbutils" "kmod-usb-printer" { ignore = { 'missing' } }
+	Install "luci-app-p910nd" "luci-i18n-p910nd-cs" "luci-i18n-p910nd-en" "kmod-usb2" "usbutils" "kmod-usb-printer" "cups" "cups-client" "hplip" "openprinting-cups-filters" "gutenprint-cups" "ghostscript" "ghostscript-fonts-std" "ghostscript-gnu-gs-fonts-other" { ignore = { 'missing' } }
 end
