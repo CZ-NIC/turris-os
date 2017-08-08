b26d4361ccfdb14ae6a62905332105cad594398e
Karel Kočí <karel.koci@nic.cz>
2017-08-08 16:00:47 +0200

    Extract turris and openwrt version from packages to bin

---
+++ scripts/unpack-version.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Copyright (C) 2017 CZ.NIC
+# Unpack Turris OS and OpenWRT version from packages
+
+SOURCE="$1"
+DESTINATION="$2"
+
+OPENWRT_PKG=base/base-files_*.ipk
+OPENWRT_VERSION=./etc/openwrt_version
+OPENWRT_RELEASE=./etc/openwrt_release
+
+TURRIS_PKG=base/turris-version_*.ipk
+TURRIS_VERSION=./etc/turris-version
+
+unpack_files() {
+	local PKG=`realpath "$SOURCE"`/"$1"
+	local DIR="$2"
+	local FILES="${@:3}"
+	(cd "$DIR" && tar -xzOf $PKG ./data.tar.gz | tar -xz $FILES)
+}
+
+TMPDIR=`mktemp -d`
+unpack_files $OPENWRT_PKG "$TMPDIR" "$OPENWRT_VERSION" "$OPENWRT_RELEASE"
+unpack_files $TURRIS_PKG "$TMPDIR" "$TURRIS_VERSION"
+mv "$TMPDIR"/"$OPENWRT_VERSION" "$TMPDIR"/"$OPENWRT_RELEASE" "$TMPDIR"/"$TURRIS_VERSION" "$DESTINATION"
+rm -rf "$TMPDIR"
