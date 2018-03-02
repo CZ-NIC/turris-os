54f5aabfe4e4a60c41e883707d6381a3f8cc0d78
Michal Hrušecký <michal.hrusecky@nic.cz>
2018-03-02 11:25:14 +0100

    base-files: Don't fail on stopping services we are about to remove

--- package/base-files/files/lib/functions.sh
+++ package/base-files/files/lib/functions.sh
@@ -169,8 +169,8 @@ default_prerm() {
 	name=$(basename ${1%.*})
 	[ -f /usr/lib/opkg/info/${name}.prerm-pkg ] && . /usr/lib/opkg/info/${name}.prerm-pkg
 	for i in `cat /usr/lib/opkg/info/${name}.list | grep "^/etc/init.d/"`; do
-		$i disable
-		$i stop
+		$i disable || :
+		$i stop || :
 	done
 }
 
