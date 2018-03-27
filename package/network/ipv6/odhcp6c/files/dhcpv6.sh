ade83a831a7ebda828245ed8bedf1031f20eac5d
Stepan Henek <stepan.henek@nic.cz>
2018-03-27 19:27:25 +0200

    odhcp6c: use upstream version + export DUID to /var/run/odhcp6c-duid.{ifname}

--- package/network/ipv6/odhcp6c/files/dhcpv6.sh
+++ package/network/ipv6/odhcp6c/files/dhcpv6.sh
@@ -11,10 +11,12 @@ proto_dhcpv6_init_config() {
 	proto_config_add_string 'reqprefix:or("auto","no",range(0, 64))'
 	proto_config_add_string clientid
 	proto_config_add_string 'reqopts:list(uinteger)'
+	proto_config_add_string 'defaultreqopts:bool'
 	proto_config_add_string 'noslaaconly:bool'
 	proto_config_add_string 'forceprefix:bool'
+	proto_config_add_string 'extendprefix:bool'
 	proto_config_add_string 'norelease:bool'
-	proto_config_add_string 'ip6prefix:ip6addr'
+	proto_config_add_array 'ip6prefix:list(ip6addr)'
 	proto_config_add_string iface_dslite
 	proto_config_add_string zone_dslite
 	proto_config_add_string iface_map
@@ -25,18 +27,30 @@ proto_dhcpv6_init_config() {
 	proto_config_add_string 'ifaceid:ip6addr'
 	proto_config_add_string "userclass"
 	proto_config_add_string "vendorclass"
+	proto_config_add_array "sendopts:list(string)"
 	proto_config_add_boolean delegate
 	proto_config_add_int "soltimeout"
 	proto_config_add_boolean fakeroutes
+	proto_config_add_boolean sourcefilter
+	proto_config_add_boolean keep_ra_dnslifetime
+	proto_config_add_int "ra_holdoff"
+}
+
+proto_dhcpv6_add_prefix() {
+	append "$3" "$1"
+}
+
+proto_dhcpv6_add_sendopts() {
+	[ -n "$1" ] && append "$3" "-x$1"
 }
 
 proto_dhcpv6_setup() {
 	local config="$1"
 	local iface="$2"
 
-	local reqaddress reqprefix clientid reqopts noslaaconly forceprefix norelease ip6prefix iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes
-	json_get_vars reqaddress reqprefix clientid reqopts noslaaconly forceprefix norelease ip6prefix iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes
-
+	local reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease ip6prefix ip6prefixes iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass sendopts delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff
+	json_get_vars reqaddress reqprefix clientid reqopts defaultreqopts noslaaconly forceprefix extendprefix norelease iface_dslite iface_map iface_464xlat ifaceid userclass vendorclass delegate zone_dslite zone_map zone_464xlat zone soltimeout fakeroutes sourcefilter keep_ra_dnslifetime ra_holdoff
+	json_for_each_item proto_dhcpv6_add_prefix ip6prefix ip6prefixes
 
 	# Configure
 	local opts=""
@@ -46,6 +60,8 @@ proto_dhcpv6_setup() {
 	[ "$reqprefix" != "no" ] && append opts "-P$reqprefix"
 
 	[ -n "$clientid" ] && append opts "-c$clientid"
+
+	[ "$defaultreqopts" = "0" ] && append opts "-R"
 
 	[ "$noslaaconly" = "1" ] && append opts "-S"
 
@@ -59,13 +75,20 @@ proto_dhcpv6_setup() {
 
 	[ -n "$userclass" ] && append opts "-u$userclass"
 
+	[ "$keep_ra_dnslifetime" = "1" ] && append opts "-L"
+
+	[ -n "$ra_holdoff" ] && append opts "-m$ra_holdoff"
+
+	local opt
 	for opt in $reqopts; do
 		append opts "-r$opt"
 	done
 
+	json_for_each_item proto_dhcpv6_add_sendopts sendopts opts
+
 	append opts "-t${soltimeout:-120}"
 
-	[ -n "$ip6prefix" ] && proto_export "USERPREFIX=$ip6prefix"
+	[ -n "$ip6prefixes" ] && proto_export "USERPREFIX=$ip6prefixes"
 	[ -n "$iface_dslite" ] && proto_export "IFACE_DSLITE=$iface_dslite"
 	[ -n "$iface_map" ] && proto_export "IFACE_MAP=$iface_map"
 	[ -n "$iface_464xlat" ] && proto_export "IFACE_464XLAT=$iface_464xlat"
@@ -76,6 +99,8 @@ proto_dhcpv6_setup() {
 	[ -n "$zone_464xlat" ] && proto_export "ZONE_464XLAT=$zone_464xlat"
 	[ -n "$zone" ] && proto_export "ZONE=$zone"
 	[ "$fakeroutes" != "0" ] && proto_export "FAKE_ROUTES=1"
+	[ "$sourcefilter" = "0" ] && proto_export "NOSOURCEFILTER=1"
+	[ "$extendprefix" = "1" ] && proto_export "EXTENDPREFIX=1"
 
 	proto_export "INTERFACE=$config"
 	proto_run_command "$config" odhcp6c \
